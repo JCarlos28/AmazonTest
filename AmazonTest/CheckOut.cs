a1c743f477823902526da0bbfafc590bd19fbf05
JCarlos28 <juancalmonte28@hotmail.com>
2017-03-27 12:05:21 -0300

    Update CheckOut.cs

--- AmazonTest/CheckOut.cs
+++ AmazonTest/CheckOut.cs
@@ -11,9 +11,16 @@ namespace AmazonTest
     {
         public static void PressAdd()
         {
-            Driver.Instance.FindElement(By.Id("add-to-cart-button")).Click();
+             Driver.Instance.FindElement(By.Id("add-to-cart-button")).Click();
             Driver.Instance.FindElement(By.Id("hlb-ptc-btn-native")).Click();
             Driver.Instance.FindElement(By.Name("enterAddressFullName")).SendKeys(InputVariables.fullName);
+            Driver.Instance.FindElement(By.Name("enterAddressAddressLine1")).SendKeys(InputVariables.AddresLine1);
+            Driver.Instance.FindElement(By.Name("enterAddressAddressLine2")).SendKeys(InputVariables.AddresLine2);
+            Driver.Instance.FindElement(By.Name("enterAddressCity")).SendKeys(InputVariables.State);
+            Driver.Instance.FindElement(By.Name("enterAddressStateOrRegion")).SendKeys(InputVariables.State);
+            Driver.Instance.FindElement(By.Name("enterAddressPostalCode")).SendKeys(InputVariables.Zip);
+            
+            new SelectElement(Driver.Instance.FindElement(By.Id("enterAddressCountryCode"))).SelectByText("Dominican Republic");
             
 
         }
